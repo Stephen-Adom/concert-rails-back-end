ae5607c21ca15e5f81a97ed44bda72c6edc68ac1
Amine derbal <aminedoran31@gmail.com>
2023-07-13 15:32:13 +0100

    Update files to fix rubocop errors

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,6 @@
 class City < ApplicationRecord
-
   validates :name, presence: true
 
-  has_many :concert_halls, class_name: "ConcertHall", foreign_key: "city_id"
+  has_many :concert_halls, class_name: 'ConcertHall', foreign_key: 'city_id'
   has_many :concerts, through: :concert_halls
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :username, presence: true , uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
-  has_many :reservations, dependent: :destroy,class_name: "Reservation", foreign_key: "user_id"
-
+  has_many :reservations, dependent: :destroy, class_name: 'Reservation', foreign_key: 'user_id'
 end
