54df7d4b8ef31858c112701b5ab64f135c09f8af
Amine derbal <aminedoran31@gmail.com>
2023-07-13 15:28:35 +0100

    Add Migration for All Tables

---
+++ db/migrate/20230713131143_create_concerts.rb
@@ -0,0 +1,15 @@
+class CreateConcerts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :concerts do |t|
+      t.string :name
+      t.string :band
+      t.text :description
+      t.integer :total_seats
+      t.integer :reserved_seats
+      t.string :artist
+      t.text :image
+
+      t.timestamps
+    end
+  end
+end
