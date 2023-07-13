b514e693d4aff6758e79cf77e902add29351dbd7
Amine derbal <aminedoran31@gmail.com>
2023-07-13 15:29:06 +0100

    Add models for all tables

---
+++ app/models/reservation.rb
@@ -0,0 +1,7 @@
+class Reservation < ApplicationRecord
+
+  belongs_to :user,class_name: "User", foreign_key: "user_id"
+  belongs_to :concert_hall,class_name: "ConcertHall", foreign_key: "concert_hall_id"
+
+
+end
