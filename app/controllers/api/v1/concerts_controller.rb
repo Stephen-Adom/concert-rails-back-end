f603c1a6841fc1fc8b21aab6044ae192ed4dc0ae
Amine derbal <aminedoran31@gmail.com>
2023-07-14 10:42:18 +0100

    Create API end point for concert details

--- app/controllers/api/v1/concerts_controller.rb
+++ app/controllers/api/v1/concerts_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::ConcertsController < ApplicationController
     concerts = Concert.all
     render json: concerts
   end
+
+  def show
+    @concert = Concert.includes(:concert_halls).find(params[:id])
+
+    render json: @concert.to_json(include: {concert_halls: {include: :city}})
+  
+  end
 end
