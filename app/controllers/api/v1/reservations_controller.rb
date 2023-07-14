9c9aab41144b484e60e8a1a78872526106162f6d
codedit334 <oussama7elabdioui@gmail.com>
2023-07-14 22:14:07 +0100

    Run rubocop*2

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -4,19 +4,19 @@ class Api::V1::ReservationsController < ApplicationController
   def index
     reservations = @current_user.reservations
 
-    if reservations.empty?
-      reservations_data = []
-    else
-      reservations_data = reservations.map do |reservation|
-        {
-          id: reservation.id,
-          name: @current_user.name,
-          concert_name: reservation.concert_hall.concert.name,
-          concert_date: reservation.concert_hall.date,
-          city_name: reservation.concert_hall.city.name
-        }
-      end
-    end
+    reservations_data = if reservations.empty?
+                          []
+                        else
+                          reservations.map do |reservation|
+                            {
+                              id: reservation.id,
+                              name: @current_user.name,
+                              concert_name: reservation.concert_hall.concert.name,
+                              concert_date: reservation.concert_hall.date,
+                              city_name: reservation.concert_hall.city.name
+                            }
+                          end
+                        end
 
     render json: { reservations: reservations_data }
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::API
-    
-    private
+  private
 
-    def authenticate_user_by_token!
-      token = request.headers['Authorization']&.split(' ')&.last
-      @current_user = User.find_by(authentication_token: token)
-  
-      if token.blank? || @current_user.nil?
-        render json: { error: 'Invalid credentials' }, status: :unauthorized
-      end
-    end
+  def authenticate_user_by_token!
+    token = request.headers['Authorization']&.split(' ')&.last
+    @current_user = User.find_by(authentication_token: token)
+
+    return unless token.blank? || @current_user.nil?
+
+    render json: { error: 'Invalid credentials' }, status: :unauthorized
+  end
 end
