93ad08f3fcab43e7bb32b183d566c0e9e2c73461
Amine derbal <aminedoran31@gmail.com>
2023-07-16 15:06:27 +0100

    Update files to fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,12 @@
 class ApplicationController < ActionController::API
-    
-    private
+  private
 
-    def authenticate_user_by_token!
-      token = request.headers['Authorization']&.split(' ')&.last
-      @current_user = User.find_by(authentication_token: token)
-  
-      if token.blank? || @current_user.nil?
-        render json: { error: 'Invalid credentials' }, status: :unauthorized
-      else
-        render json: { message: "Welcome, #{current_user.name}!" }
-      end
-    end
+  def authenticate_user_by_token!
+    token = request.headers['Authorization']&.split(' ')&.last
+    @current_user = User.find_by(authentication_token: token)
+
+    return unless token.blank? || @current_user.nil?
+
+    render json: { error: 'Invalid credentials' }, status: :unauthorized
+  end
 end
