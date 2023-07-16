bebe3cdde7b517964e268938553d843647792cda
Amine derbal <aminedoran31@gmail.com>
2023-07-16 15:06:09 +0100

    Implement create and derstroy methods for concerts

--- app/controllers/api/v1/concerts_controller.rb
+++ app/controllers/api/v1/concerts_controller.rb
@@ -1,6 +1,43 @@
 class Api::V1::ConcertsController < ApplicationController
+  before_action :authenticate_user_by_token!, only: %i[create destroy]
   def index
     concerts = Concert.all
     render json: concerts
+  end
+
+  def create
+    if Concert.find_by(name: concert_params[:name])
+      render json: { error: 'Concert already exists' }, status: :unprocessable_entity
+    else
+      concert = Concert.new(concert_params)
+      if concert.save
+        concert.concert_halls.create(concert_hall_params)
+        render json: concert, status: :created
+      else
+        render json: concert.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def destroy
+    if Concert.find_by(id: params[:id])
+      concert = Concert.find(params[:id])
+      concert.reservations.destroy_all if concert.reservations.any?
+      concert.concert_halls.destroy_all if concert.concert_halls.any?
+      concert.destroy
+      render json: { message: 'Concert deleted' }, status: :ok
+    else
+      render json: { error: 'Concert not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def concert_params
+    params.require(:concert).permit(:name, :band, :image, :description, :total_seats, :artist)
+  end
+
+  def concert_hall_params
+    params.require(:concert_hall).permit(:city_id, :date)
   end
 end
