78365dbbd4358db8efb04112eb5b5670426e46ee
Amine derbal <aminedoran31@gmail.com>
2023-07-16 15:04:57 +0100

    Update model associations

--- app/models/concert_hall.rb
+++ app/models/concert_hall.rb
@@ -3,4 +3,5 @@ class ConcertHall < ApplicationRecord
 
   belongs_to :city, class_name: 'City', foreign_key: 'city_id'
   belongs_to :concert, class_name: 'Concert', foreign_key: 'concert_id'
+  has_many :reservations, class_name: 'Reservation', foreign_key: 'concert_hall_id'
 end
