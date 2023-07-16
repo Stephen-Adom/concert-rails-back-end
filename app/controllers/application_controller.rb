570fd81cd94b7eace3f01157e785711dc3b863ed
codedit334 <oussama7elabdioui@gmail.com>
2023-07-16 17:07:53 +0100

    Fix authenticate_user_by_token! error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,8 @@ class ApplicationController < ActionController::API
     token = request.headers['Authorization']&.split(' ')&.last
     @current_user = User.find_by(authentication_token: token)
 
-<<<<<<< HEAD
     return unless token.blank? || @current_user.nil?
 
     render json: { error: 'Invalid credentials' }, status: :unauthorized
-=======
-    if token.blank? || @current_user.nil?
-      render json: { error: 'Invalid credentials' }, status: :unauthorized
-    else
-      render json: { message: "Welcome, #{current_user.name}!" }
-    end
->>>>>>> 9d3ce4105ca99771f3dafd683ea742b46dfaf93c
   end
 end
