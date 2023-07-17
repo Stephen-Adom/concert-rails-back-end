848e742e171366ff1194935ca10b863d94d428f5
codedit334 <oussama7elabdioui@gmail.com>
2023-07-17 11:18:22 +0100

    Display name and id of user on authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: { authentication_token: user.authentication_token }, status: :created
+      render json: { authentication_token: user.authentication_token, user_id: user.id, name: user.name }, status: :created
     else
       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
     end
