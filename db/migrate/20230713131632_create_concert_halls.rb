0dfb1e4271d6887b1185c21ba50abb00004933b5
Amine derbal <aminedoran31@gmail.com>
2023-07-17 10:41:31 +0100

    Update tables of the database

--- db/migrate/20230713131632_create_concert_halls.rb
+++ db/migrate/20230713131632_create_concert_halls.rb
@@ -2,8 +2,11 @@ class CreateConcertHalls < ActiveRecord::Migration[7.0]
   def change
     create_table :concert_halls do |t|
       t.integer :concert_id
-      t.integer :city_id
-      t.date :date
+      t.string :hall_name
+      t.text :city_name
+      t.datetime :date
+      t.integer :total_seats
+      t.integer :reserved_seats
 
       t.timestamps
     end
