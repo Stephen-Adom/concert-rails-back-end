5cf7edef3fc1d68228dcd3e569ad7eca0ff81907
codedit334 <oussama7elabdioui@gmail.com>
2023-07-17 11:19:04 +0100

    Run rubocop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user&.authenticate(params[:password])
-      render json: { authentication_token: user.authentication_token, user_id: user.id, name: user.name  }, status: :ok
+      render json: { authentication_token: user.authentication_token, user_id: user.id, name: user.name }, status: :ok
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
