0d976a17651626344d6ca46697c2a766820aa0f8
Amine derbal <aminedoran31@gmail.com>
2023-07-19 15:40:05 +0100

    Fix bug

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::SessionsController < ApplicationController
   before_action :create_admin_user, only: :create
   def create
     user = User.find_by(username: params[:username])
-    user_info = user.send_user_information
-
+    
     if user&.authenticate(params[:password])
+      user_info = user.send_user_information
       render json: user_info, status: :ok
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
