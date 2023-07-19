47981162423ddce4896a1891336f4d4783b8fcd5
codedit334 <oussama7elabdioui@gmail.com>
2023-07-19 13:42:31 +0100

    User can only make one reservation to a concert_hall

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -25,19 +25,27 @@ class Api::V1::ReservationsController < ApplicationController
 
   def create
     concert_hall = ConcertHall.find(params[:reservation][:concert_hall_id])
-
+    user = @current_user 
+  
     if concert_hall.total_seats > concert_hall.reserved_seats
-      reservation = Reservation.new(reservation_params)
-      if reservation.save
-        concert_hall.increment!(:reserved_seats)
-        render json: { message: 'Reservation created successfully' }, status: :created
+      if user.reservations.where(concert_hall: concert_hall).exists?
+        render json: { error: 'You have already made a reservation at this concert hall' }, status: :unprocessable_entity
       else
-        render json: { error: 'Failed to create reservation' }, status: :unprocessable_entity
+        reservation = Reservation.new(reservation_params)
+        reservation.user = user
+  
+        if reservation.save
+          concert_hall.increment!(:reserved_seats)
+          render json: { message: 'Reservation created successfully' }, status: :created
+        else
+          render json: { error: 'Failed to create reservation' }, status: :unprocessable_entity
+        end
       end
     else
       render json: { error: 'Not enough seats available in the concert hall' }, status: :unprocessable_entity
     end
   end
+  
 
   private
 
