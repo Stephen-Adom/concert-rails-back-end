0ce88cb3b007154299117b04548707b34422652b
Amine derbal <aminedoran31@gmail.com>
2023-07-24 11:51:15 +0100

    Delete test folder

--- test/controllers/api/v1/concerts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Api::V1::ConcertsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get api_v1_concerts_index_url
-    assert_response :success
-  end
-end
